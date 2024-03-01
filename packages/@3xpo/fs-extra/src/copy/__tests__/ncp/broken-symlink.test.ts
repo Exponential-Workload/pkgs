5620857511350ee25448658f054a147cb83882ad
Expo <expo@expo.moe>
2024-03-01 18:59:16 +0100

    fix: avoid using done in broken-symlink

--- packages/@3xpo/fs-extra/src/copy/__tests__/ncp/broken-symlink.test.ts
+++ packages/@3xpo/fs-extra/src/copy/__tests__/ncp/broken-symlink.test.ts
@@ -5,51 +5,15 @@ import * as os from 'os';
 import fse from '../../..';
 import { copy as ncp } from '../../';
 import path from 'path';
-import assert from 'assert';
+import type { URL } from 'url';
 
-/* global afterEach, beforeEach, describe, it */
-
-describe('ncp broken symlink', () => {
-  const TEST_DIR = path.join(
-    os.tmpdir(),
-    'fs-extra-test-suite',
-    'ncp-broken-symlinks',
-  );
-  const src = path.join(TEST_DIR, 'src');
-  const out = path.join(TEST_DIR, 'out');
-
-  beforeEach(() => {
-    return fse
-      .emptyDir(TEST_DIR)
-      .then(
-        () =>
-          new Promise((rs, rj) =>
-            createFixtures(src, e => (e ? rj(e) : rs(void 0))),
-          ),
-      );
-  });
-
-  afterEach(() => fs.rmSync(TEST_DIR, { recursive: true, force: true }));
-
-  it('should not error if symlink is broken', done => {
-    ncp(src, out, err => {
-      expect(err).toBe(null);
-      done();
-    });
-  });
-
-  it('should return an error if symlink is broken and dereference=true', done => {
-    ncp(src, out, { dereference: true }, err => {
-      expect(err.code).toBe('ENOENT');
-      done();
-    });
-  });
-});
-
-function createFixtures(srcDir, callback) {
+const createFixtures = (
+  srcDir: fs.PathLike,
+  callback: string | ((arg0: NodeJS.ErrnoException) => void),
+) => {
   fs.mkdir(srcDir, err => {
-    let brokenFile;
-    let brokenFileLink;
+    let brokenFile: string | number | Buffer | URL;
+    let brokenFileLink: fs.PathLike;
 
     if (err) return callback(err);
 
@@ -65,4 +29,55 @@ function createFixtures(srcDir, callback) {
     // break the symlink now
     fse.remove(brokenFile, callback);
   });
-}
+};
+
+/* global afterEach, beforeEach, describe, it */
+
+describe('ncp broken symlink', () => {
+  const TEST_DIR = path.join(
+    os.tmpdir(),
+    'fs-extra-test-suite',
+    'ncp-broken-symlinks',
+  );
+  const src = path.join(TEST_DIR, 'src');
+  const out = path.join(TEST_DIR, 'out');
+
+  beforeEach(async () => {
+    await fse.emptyDir(TEST_DIR);
+    return await new Promise((rs, rj) =>
+      createFixtures(src, e => (e ? rj(e) : rs(void 0))),
+    );
+  });
+
+  afterEach(() => fs.rmSync(TEST_DIR, { recursive: true, force: true }));
+
+  it('should not error if symlink is broken', async () => {
+    const rt = await ncp(src, out)
+      .then(v => ({
+        err: false,
+        val: v,
+      }))
+      .catch(e => ({
+        err: true,
+        val: e,
+      }));
+
+    expect(rt.err).toBe(false);
+    expect(rt.val).toBe(void 0);
+  });
+
+  it('should return an error if symlink is broken and dereference=true', async () => {
+    const rt = await ncp(src, out, { dereference: true })
+      .then(v => ({
+        err: false,
+        val: v,
+      }))
+      .catch(e => ({
+        err: true,
+        val: e,
+      }));
+
+    expect(rt.err).toBe(true);
+    expect(rt.val.code).toBe('ENOENT');
+  });
+});
